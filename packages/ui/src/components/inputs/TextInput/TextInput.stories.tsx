a1b2755bee453de7138b08ee414924385c2d346a
Simon Ledunois <simon.ledunois@cgi.com>
2024-03-01 10:47:45 +0100

    feat: update input text

--- packages/ui/src/components/inputs/TextInput/TextInput.stories.tsx
+++ packages/ui/src/components/inputs/TextInput/TextInput.stories.tsx
@@ -6,29 +6,67 @@ const stories: Meta<StoryFn<any>> = {
   title: "Components/Inputs/TextInput",
   component: TextInput,
   argTypes: {
-    // Définissez ici vos paramètres d'histoires spécifiques aux arguments (args)
+    label: {
+      control: 'text',
+      description: 'input label',
+      table: {
+        type: { summary: 'text' },
+      }
+    },
+    disabled: {
+      control: 'boolean',
+      description: 'input disabled state',
+      table: {
+        defaultValue: { summary: false },
+        type: { summary: 'boolean' },
+      }
+    },
+    variant: {
+      control: 'select',
+      description: 'input rendering style',
+      options: ['standard', 'outlined', 'filled'],
+      table: {
+        defaultValue: { summary: 'standard' },
+        type: { summary: 'string' },
+      }
+    },
+    type: {
+      control: 'select',
+      description: 'input type',
+      options: ['text', 'number', 'email', 'password'],
+      table: {
+        defaultValue: { summary: 'text' },
+        type: { summary: 'string' },
+      },
+    },
   },
 };
 
 const Template: StoryFn<any> = (args) => <TextInput {...args} />;
 
+export const Default: any = Template.bind({});
+Default.args = {
+  label: "Default input",
+  disabled: false,
+};
+
 export const Standard: any = Template.bind({});
 Standard.args = {
-  label: "Champ",
+  label: "Standard input",
   disabled: false,
   variant: "standard"
 };
 
 export const Outlined: any = Template.bind({});
 Outlined.args = {
-  label: "Champ",
+  label: "Outlined input",
   disabled: false,
   variant: "outlined"
 };
 
 export const Filled: any = Template.bind({});
 Filled.args = {
-  label: "Champ",
+  label: "Filled input",
   disabled: false,
   variant: "filled"
 };
