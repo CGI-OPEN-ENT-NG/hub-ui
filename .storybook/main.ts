8870ea2fdb8b7b65d5b12f2a6736d16af0218fa7
Simon Ledunois <simon.ledunois@cgi.com>
2024-03-04 12:27:55 +0100

    chore: #hub-319 add mui props in storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -9,13 +9,23 @@ const config: StorybookConfig = {
     "@storybook/addon-interactions",
   ],
   typescript: {
+    check: false,
+    checkOptions: {},
     reactDocgen: "react-docgen-typescript",
     reactDocgenTypescriptOptions: {
-      compilerOptions: {
-        allowSyntheticDefaultImports: false,
-        esModuleInterop: false,
-      },
-      propFilter: () => true,
+      // speeds up storybook build time
+      // @ts-ignore
+      allowSyntheticDefaultImports: false,
+      // speeds up storybook build time
+      esModuleInterop: false,
+      // makes union prop types like variant and size appear as select controls
+      shouldExtractLiteralValuesFromEnum: true,
+      // makes string and boolean types that can be undefined appear as inputs and switches
+      shouldRemoveUndefinedFromOptional: true,
+      // Filter out third-party props from node_modules except @mui packages
+      propFilter: (prop) => prop.parent
+        ? /node_modules\/@mui/.test(prop.parent.fileName)
+        : true,
     },
   },
   framework: {
@@ -26,4 +36,16 @@ const config: StorybookConfig = {
     autodocs: true,
   },
 };
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    expanded: true, // Adds the description and default columns
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+}
+
 export default config;
--- packages/ui/src/components/inputs/TextInput/TextInput.stories.tsx
+++ packages/ui/src/components/inputs/TextInput/TextInput.stories.tsx
@@ -6,21 +6,6 @@ const stories: Meta<StoryFn<any>> = {
   title: "Components/Inputs/TextInput",
   component: TextInput,
   argTypes: {
-    label: {
-      control: 'text',
-      description: 'input label',
-      table: {
-        type: { summary: 'text' },
-      }
-    },
-    disabled: {
-      control: 'boolean',
-      description: 'input disabled state',
-      table: {
-        defaultValue: { summary: false },
-        type: { summary: 'boolean' },
-      }
-    },
     variant: {
       control: 'select',
       description: 'input rendering style',
@@ -30,15 +15,6 @@ const stories: Meta<StoryFn<any>> = {
         type: { summary: 'string' },
       }
     },
-    type: {
-      control: 'select',
-      description: 'input type',
-      options: ['text', 'number', 'email', 'password'],
-      table: {
-        defaultValue: { summary: 'text' },
-        type: { summary: 'string' },
-      },
-    },
   },
 };
 
@@ -47,20 +23,17 @@ const Template: StoryFn<any> = (args) => <TextInput {...args} />;
 export const Default: any = Template.bind({});
 Default.args = {
   label: "Default input",
-  disabled: false,
 };
 
 export const Standard: any = Template.bind({});
 Standard.args = {
   label: "Standard input",
-  disabled: false,
   variant: "standard"
 };
 
 export const Outlined: any = Template.bind({});
 Outlined.args = {
   label: "Outlined input",
-  disabled: false,
   variant: "outlined"
 };
 
