a0701816f7f9322ceeec1065acd846cb771ed4d2
Simon Ledunois <simon.ledunois@cgi.com>
2024-03-06 18:31:02 +0100

    chore: update theme mui palette

--- packages/ui/src/components/feedback/Alert/Alert.tsx
+++ packages/ui/src/components/feedback/Alert/Alert.tsx
@@ -10,30 +10,13 @@ type AlertProps = {
     title?: string
 } & Omit<MUIAlertProps, "variant" | "color">
 
-const StyledAlert = styled(MUIAlert)(({ theme }) => {
-    const hubTheme = theme as any;
+const StyledAlert = styled(MUIAlert)(() => {
     return {
-        "&.MuiAlert-standard.MuiAlert-colorInfo, &.MuiAlert-standardInfo, &.MuiAlert-standard.MuiAlert-colorInfo .MuiAlert-icon, &.MuiAlert-standardInfo .MuiAlert-icon": {
-            backgroundColor: hubTheme.palette.blue.light,
-            color: hubTheme.palette.blue.dark
-        },
-        "&.MuiAlert-standard.MuiAlert-colorSuccess, &.MuiAlert-standardSuccess, &.MuiAlert-standard.MuiAlert-colorSuccess .MuiAlert-icon, &.MuiAlert-standardSuccess .MuiAlert-icon": {
-            backgroundColor: hubTheme.palette.green.light,
-            color: hubTheme.palette.green.dark
-        },
-        "&.MuiAlert-standard.MuiAlert-colorWarning, &.MuiAlert-standardWarning, &.MuiAlert-standard.MuiAlert-colorWarning .MuiAlert-icon, &.MuiAlert-standardWarning .MuiAlert-icon": {
-            backgroundColor: hubTheme.palette.yellow.light,
-            color: hubTheme.palette.yellow.dark
-        },
-        "&.MuiAlert-standard.MuiAlert-colorError, &.MuiAlert-standardError, &.MuiAlert-standard.MuiAlert-colorError .MuiAlert-icon, &.MuiAlert-standardError .MuiAlert-icon": {
-            backgroundColor: hubTheme.palette.red.light,
-            color: hubTheme.palette.red.dark
-        },
         "& .MuiAlertTitle-root": {
             fontWeight: 600
         }
     }
-})
+});
 
 const Alert: React.FunctionComponent<AlertProps> = (props) => {
     return <StyledAlert {...props} severity={props.severity} color={props.severity} variant="standard">
--- packages/ui/src/components/inputs/TextInput/TextInput.tsx
+++ packages/ui/src/components/inputs/TextInput/TextInput.tsx
@@ -1,23 +1,11 @@
 import TextField, { type TextFieldProps } from "@mui/material/TextField";
-import { styled } from "@mui/material/styles";
 
-export type InputTextProps = {
-
-} & TextFieldProps
-
-const StyledTextField = styled(TextField)(({ theme }) => ({
-  "& label.Mui-focused": {
-    color: theme.palette.primary.main,
-  },
-  "& .MuiTextInput-underline:after": {
-    borderBottomColor: theme.palette.primary.main,
-  },
-}));
+export type InputTextProps = {} & TextFieldProps
 
 const TextInput: React.FunctionComponent<InputTextProps> = (
   props: InputTextProps
 ) => {
-  return <StyledTextField {...props} variant={props.variant || "standard"} />;
+  return <TextField {...props} variant={props.variant || "standard"} />;
 };
 
 export default TextInput;
