7d39a00e37a2262978d94f80b1c1588b52234882
Guillaume Roux <guillaume.roux@cgi.com>
2024-07-25 15:41:31 +0200

    feat(ui): change DefaultButton style

--- packages/ui/src/components/buttons/DefaultButton/DefaultButton.tsx
+++ packages/ui/src/components/buttons/DefaultButton/DefaultButton.tsx
@@ -8,48 +8,60 @@ export interface DefaultButtonProps extends LoadingButtonProps {
   text?: string;
 }
 
-const StyledButton = styled(LoadingButton)<DefaultButtonProps>(({ color, theme }) => ({
-  justifyContent: "center",
-  alignItems: "center",
-  columnGap: 8,
-  minHeight: 38,
-  paddingLeft: 12,
-  paddingRight: 12,
-  textTransform: "none",
-  color: color,
-  borderColor: color,
-  borderRadius: 5,
-  borderWidth: 2,
-  transition: "all 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms",
-  "&.MuiButton-contained": {
-    color: theme.palette.common.white,
-  },
-  "&.MuiButton-outlined": {
+const StyledButton = styled(LoadingButton)<DefaultButtonProps>(
+  ({ color, theme }) => ({
+    justifyContent: "center",
+    alignItems: "center",
+    columnGap: 8,
+    minHeight: 38,
+    paddingLeft: 12,
+    paddingRight: 12,
+    textTransform: "none",
+    color: color,
+    borderColor: color,
     borderRadius: 5,
     borderWidth: 2,
-    "&.MuiButton-outlinedPrimary": {
-      "&:active": {
-        borderColor: theme.palette.primary.main,
-        color: theme.palette.primary.main,
-      },
-      "&:hover": {
-        backgroundColor: theme.palette.primary.dark,
-        borderColor: theme.palette.primary.dark,
-        color: theme.palette.common.white,
-      }
+    transition: "all 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms",
+    "&.MuiButton-contained": {
+      color: theme.palette.common.white,
     },
-    "&.MuiButton-outlinedSecondary": {
-      "&:active": {
-        borderColor: theme.palette.secondary.main,
-        color: theme.palette.secondary.main,
+    "&.MuiButton-outlined": {
+      borderRadius: 5,
+      borderWidth: 2,
+      "&.MuiButton-colorPrimary": {
+        "&:active": {
+          borderColor: theme.palette.primary.main,
+          color: theme.palette.primary.main,
+        },
+        "&:hover": {
+          backgroundColor: theme.palette.primary.dark,
+          borderColor: theme.palette.primary.dark,
+          color: theme.palette.common.white,
+        },
       },
-      "&:hover": {
-        backgroundColor: theme.palette.secondary.main,
-        color: theme.palette.common.white,
-      }
-    }
-  }
-}));
+      "&.MuiButton-colorSecondary": {
+        "&:active": {
+          borderColor: theme.palette.secondary.main,
+          color: theme.palette.secondary.main,
+        },
+        "&:hover": {
+          backgroundColor: theme.palette.secondary.main,
+          color: theme.palette.common.white,
+        },
+      },
+      "&.MuiButton-colorError": {
+        "&:active": {
+          borderColor: theme.palette.error.main,
+          color: theme.palette.error.main,
+        },
+        "&:hover": {
+          backgroundColor: theme.palette.error.main,
+          color: theme.palette.common.white,
+        },
+      },
+    },
+  })
+);
 
 const DefaultButton: React.FunctionComponent<DefaultButtonProps> = ({
   icon,
@@ -59,10 +71,7 @@ const DefaultButton: React.FunctionComponent<DefaultButtonProps> = ({
   const color = otherProps.color ?? "primary";
 
   return (
-    <StyledButton
-      {...otherProps}
-      color={color}
-    >
+    <StyledButton {...otherProps} color={color}>
       {icon?.position === "left" ? <ButtonIcon {...icon} /> : null}
       {text}
       {icon?.position === "right" ? <ButtonIcon {...icon} /> : null}
