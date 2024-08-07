e8889d5d9f3f0c489b3bb7a3187859c49cfbb4ce
Guillaume Roux <guillaume.roux@cgi.com>
2024-08-07 17:09:57 +0200

    feat(local-ui): add example component with theme usage

--- apps/local-ui/src/components/ExampleComponent.tsx
+++ apps/local-ui/src/components/ExampleComponent.tsx
@@ -1,11 +1,32 @@
-import React from 'react';
+import { useTheme } from "@mui/material";
+import type {} from "@mui/material/themeCssVarsAugmentation"; // necessary to use theme.vars
+import React from "react";
+import "./example.css";
 
 interface ExampleComponentProps {
-  text: string;
+  primaryText: string;
+  secondaryText: string;
 }
 
-const ExampleComponent: React.FC<ExampleComponentProps> = ({ text }) => {
-  return <div>{text}</div>;
+const ExampleComponent: React.FC<ExampleComponentProps> = ({
+  primaryText,
+  secondaryText,
+}) => {
+  const theme = useTheme();
+
+  return (
+    <div
+      style={{
+        padding: 10,
+        backgroundColor: theme.vars.palette.primary.lighter,
+      }}
+    >
+      <p style={{ color: "var(--theme-palette-primary-main)" }}>
+        {primaryText}
+      </p>
+      <p className="secondary-text">{secondaryText}</p>
+    </div>
+  );
 };
 
 export default ExampleComponent;
