371f0c2cb9157399d2e8ee54ba48c1c7b9d9a58a
Guillaume Roux <guillaume.roux@cgi.com>
2024-09-05 16:06:59 +0200

    feat(ui): set LoaderBackdrop to primary color

--- packages/ui/src/components/LoaderBackdrop/LoaderBackdrop.tsx
+++ packages/ui/src/components/LoaderBackdrop/LoaderBackdrop.tsx
@@ -1,17 +1,18 @@
+import Box from "@mui/material/Box";
 import "./styles.css";
 
 export interface LoaderBackdropProps {}
 
 const LoaderBackdrop: React.FunctionComponent<LoaderBackdropProps> = ({}) => {
   return (
-    <div className="cs-loader-backdrop">
-      <div className="cs-loader-backdrop-inner">
+    <Box className="cs-loader-backdrop" color="primary.main">
+      <Box className="cs-loader-backdrop-inner">
         <label>●</label>
         <label>●</label>
         <label>●</label>
         <label>●</label>
-      </div>
-    </div>
+      </Box>
+    </Box>
   );
 };
 
