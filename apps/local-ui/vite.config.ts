dc26b386d5127e12c951db78475f9faf4a00532b
Guillaume Roux <guillaume.roux@cgi.com>
2024-11-28 16:43:56 +0100

    chore: update dependencies

--- apps/local-ui/vite.config.ts
+++ apps/local-ui/vite.config.ts
@@ -1,26 +1,28 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tsconfigPaths from 'vite-tsconfig-paths';
-import * as path from 'path';
+import react from "@vitejs/plugin-react";
+import * as path from "path";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 const resolve = {
-  alias:{
-    '@cgi-learning-hub/ui' : path.resolve(__dirname, '../../packages/ui/src/index.ts'),
-    '@cgi-learning-hub/theme' : path.resolve(__dirname, '../../packages/theme/src/index.ts')
-  }
-}
-
-const plugins = [react(), tsconfigPaths()];
-
-const server = {
-  host: "0.0.0.0",
-  port: 3000,
-  strictPort: true
+  alias: {
+    "@cgi-learning-hub/ui": path.resolve(
+      __dirname,
+      "../../packages/ui/src/index.ts"
+    ),
+    "@cgi-learning-hub/theme": path.resolve(
+      __dirname,
+      "../../packages/theme/src/index.ts"
+    ),
+  },
 };
 
 export default defineConfig({
-    // comments resolve to try your deps built but you need to build them first (see "pnpm -r run build")
-    // resolve,
-    plugins,
-    server
+  plugins: [react(), tsconfigPaths()],
+  server: {
+    host: "0.0.0.0",
+    port: 3000,
+    strictPort: true,
+  },
+  // comments resolve to try your deps built but you need to build them first (see "pnpm -r run build")
+  // resolve,
 });
